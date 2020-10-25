949345bdb7983c7176392d5f6cf1eebd06e084c6
AlanVidal1999 <70927107+AlanVidal1999@users.noreply.github.com>
2020-10-25 16:41:57 -0600

    Add files via upload

---
+++ LDR.ino
@@ -0,0 +1,38 @@
+void setup() {
+  pinMode(A0,INPUT);
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+double pot1 = 0;
+double Van;
+double IL, A, B;
+void loop() 
+{
+  delay(1000);
+  pot1 = analogRead(A0);
+  Van= (pot1*5)/1023;
+  Serial.print("Voltaje Analogico:  ");
+  Serial.print(Van);
+  Serial.println(" V");
+  Serial.println(" ");
+  Serial.print("Numero Analogico:  ");
+  Serial.println(pot1);
+  Serial.println(" ");
+  B = (5/Van)-1;
+  A = pow(B,(-1.165501166));
+  IL= (443.6100872)*(A);
+  if(IL<=2000)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else
+  {
+    digitalWrite(13, LOW);
+  }
+  Serial.print("Intensidad Luminosa:  ");
+  Serial.print(IL);
+  Serial.println(" lux");
+  Serial.println(" ");
+  
+   //Imprimir con salto de linea
+}
