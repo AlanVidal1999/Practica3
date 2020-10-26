53295e38d0fce4b09da86d3296772471f86db5ae
AlanVidal1999 <70927107+AlanVidal1999@users.noreply.github.com>
2020-10-25 20:28:41 -0600

    Update LDR.ino

--- LDR.ino
+++ LDR.ino
@@ -8,20 +8,13 @@ double Van;
 double IL, A, B;
 void loop() 
 {
-  delay(1000);
+  
   pot1 = analogRead(A0);
   Van= (pot1*5)/1023;
-  Serial.print("Voltaje Analogico:  ");
-  Serial.print(Van);
-  Serial.println(" V");
-  Serial.println(" ");
-  Serial.print("Numero Analogico:  ");
-  Serial.println(pot1);
-  Serial.println(" ");
   B = (5/Van)-1;
   A = pow(B,(-1.165501166));
   IL= (443.6100872)*(A);
-  if(IL<=2000)
+  if(IL<2000)
   {
     digitalWrite(13, HIGH);
   }
@@ -29,10 +22,18 @@ void loop()
   {
     digitalWrite(13, LOW);
   }
+  Serial.print("Voltaje Analogico:  ");
+  Serial.print(Van);
+  Serial.println(" V");
+  Serial.println(" ");
+  Serial.print("Numero Analogico:  ");
+  Serial.println(pot1);
+  Serial.println(" ");
   Serial.print("Intensidad Luminosa:  ");
   Serial.print(IL);
   Serial.println(" lux");
   Serial.println(" ");
   
+  delay(1000);
    //Imprimir con salto de linea
 }
